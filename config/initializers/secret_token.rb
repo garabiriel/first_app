1f24c6ea44bd7103fa1592973238d2d9cf1b8806
skyhub <gabriel@skyhub.com.br>
2014-09-23 18:42:39 -0300

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '3b8562bfcad6b0fd2ee73a85f8eb77dcc4e016463697d3b6a560e63c318afceedcc4172d4fd28dbb140810ce92b847cab9435fb36279dbe4c23c2a98b9025e4a'
